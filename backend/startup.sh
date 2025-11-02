63a203cd60cafd5117a12626ddfb9ef4ddc3d3b6
Ram Jampani <rambabujampani@gmail.com>
2025-11-02 16:55:42 +0530

    modified the patch as sartup file existins under backend folder

--- backend/startup.sh
+++ backend/startup.sh
@@ -4,7 +4,7 @@ echo "📢 Working directory: $(pwd)"
 echo "✅ Starting FastAPI: main:app"
 
 # Ensure proper permissions (in case of redeploy)
-chmod +x /home/site/wwwroot/startup.sh
+chmod +x /home/site/wwwroot/backend/startup.sh
 
 # Run FastAPI with correct working directory
 exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
