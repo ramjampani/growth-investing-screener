c38f881f990d0cbbca8e551c04270a6858d30c39
Ram Jampani <rambabujampani@gmail.com>
2025-11-02 16:05:12 +0530

    Fast API is deployed directly under /home/site/wwwroot so modfiled paths in startup.sh yml file and path of startup command execution in azure

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd /home/site/wwwroot/backend
 echo "📢 Working directory: $(pwd)"
 echo "✅ Starting FastAPI: main:app"
 
 # Ensure proper permissions (in case of redeploy)
-chmod +x /home/site/wwwroot/backend/startup.sh
+chmod +x /home/site/wwwroot/startup.sh
 
 # Run FastAPI with correct working directory
 exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
